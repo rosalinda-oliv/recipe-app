42ec8afcc51dd3fd08c19e62c0c0ae7473e00f55
rosi <rosimartinsoliveira@gmail.com>
2020-07-31 12:55:01 +0100

    added mongodb schema

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,8 +6,20 @@ router.get('/', (req, res) => {
     res.send('We are on recipes');
 });
 
-router.post('/', ((req, res) => {
-   console.log(req.body);
-}));
+router.post('/', (req, res) => {
+   const recipe = new Recipe({
+       title: req.body.title,
+       ingredients: req.body.ingredients,
+       preparations: req.body.preparations
+   });
+
+   recipe.save()
+       .then( data => {
+           res.json(data);
+       })
+       .catch(err => {
+        res.json({message: err});
+    });
+});
 
 module.exports = router;
